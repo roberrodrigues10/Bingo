4924685602b2cd36a1796e76f33e5b7d0ef67157
Rober <roberrodrigues300@gmail.com>
2024-07-22 19:20:34 -0500

    bingo

---
+++ bingo.js
@@ -0,0 +1,261 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+let printResultado = "";
+let numerosB = [1,2,3,4,5];
+let numerosI = [2,2,3,4,5];
+let numerosN = [3,2,3,4,5];
+let numerosG = [4,2,3,4,5];
+let numerosO = [5,2,3,4,5];
+
+
+
+for (let numerosestaticos = 0; numerosestaticos < numerosB.length && numerosestaticos < numerosI.length && numerosestaticos < numerosN.length && numerosestaticos < numerosG.length && numerosestaticos < numerosO.length; numerosestaticos++) {
+    numeroGeneradoB = numerosB[numerosestaticos];
+    numeroGeneradoI = numerosI[numerosestaticos];
+    numeroGeneradoN = numerosN[numerosestaticos];
+    numeroGeneradoG = numerosG[numerosestaticos];
+    numeroGeneradoO = numerosO[numerosestaticos];
+
+
+
+    printResultado+=    '<div class="row">'
+    printResultado+=        '<div class="col-2">'
+    printResultado+=          '<div class="b">'
+    printResultado+=            '<div class="numero">'+numeroGeneradoB+'</div>'
+    printResultado+=           '</div>'
+    printResultado+=        '</div>'
+    printResultado+=        '<div class="col-2">'
+    printResultado+=          '<div class="i">'
+    printResultado+=            '<div class="numero">'+numeroGeneradoI+'</div>'
+    printResultado+=           '</div>'
+    printResultado+=        '</div>'
+    printResultado+=        '<div class="col-2">'
+    printResultado+=          '<div class="n">'
+    printResultado+=            '<div class="numero">'+numeroGeneradoN+'</div>'
+    printResultado+=           '</div>'
+    printResultado+=        '</div>'
+    printResultado+=        '<div class="col-2">'
+    printResultado+=          '<div class="g">'
+    printResultado+=            '<div class="numero">'+numeroGeneradoG+'</div>'
+    printResultado+=           '</div>'
+    printResultado+=        '</div>'
+    printResultado+=        '<div class="col-2">'
+    printResultado+=          '<div class="o">'
+    printResultado+=            '<div class="numero">'+numeroGeneradoO+'</div>'
+    printResultado+=           '</div>'
+    printResultado+=        '</div>'
+    printResultado+=    '</div>'
+
+    
+}   
+document.getElementById('B').innerHTML = printResultado
+});
+document.addEventListener('DOMContentLoaded', function(){
+
+    let printResultado = "";
+    let numerosB = [1,2,3,4,5];
+    let numerosI = [2,2,3,4,5];
+    let numerosN = [3,2,3,4,5];
+    let numerosG = [4,2,3,4,5];
+    let numerosO = [5,2,3,4,5];
+    
+    
+    
+    for (let numerosestaticos = 0; numerosestaticos < numerosB.length && numerosestaticos < numerosI.length && numerosestaticos < numerosN.length && numerosestaticos < numerosG.length && numerosestaticos < numerosO.length; numerosestaticos++) {
+        numeroGeneradoB = numerosB[numerosestaticos];
+        numeroGeneradoI = numerosI[numerosestaticos];
+        numeroGeneradoN = numerosN[numerosestaticos];
+        numeroGeneradoG = numerosG[numerosestaticos];
+        numeroGeneradoO = numerosO[numerosestaticos];
+    
+    
+    
+        printResultado+=    '<div class="row">'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="b colortachar">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoB+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="i">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoI+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="n">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoN+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="g">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoG+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="o">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoO+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=    '</div>'
+    
+        
+    }   
+    document.getElementById('I').innerHTML = printResultado 
+});
+document.addEventListener('DOMContentLoaded', function(){
+
+    let printResultado = "";
+    let numerosB = [1,2,3,4,5];
+    let numerosI = [2,2,3,4,5];
+    let numerosN = [3,2,3,4,5];
+    let numerosG = [4,2,3,4,5];
+    let numerosO = [5,2,3,4,5];
+    
+    
+    
+    for (let numerosestaticos = 0; numerosestaticos < numerosB.length && numerosestaticos < numerosI.length && numerosestaticos < numerosN.length && numerosestaticos < numerosG.length && numerosestaticos < numerosO.length; numerosestaticos++) {
+        numeroGeneradoB = numerosB[numerosestaticos];
+        numeroGeneradoI = numerosI[numerosestaticos];
+        numeroGeneradoN = numerosN[numerosestaticos];
+        numeroGeneradoG = numerosG[numerosestaticos];
+        numeroGeneradoO = numerosO[numerosestaticos];
+    
+    
+    
+        printResultado+=    '<div class="row">'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="b">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoB+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="i colortachar">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoI+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="n">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoN+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="g">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoG+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="o">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoO+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=    '</div>'
+    
+        
+    }   
+    document.getElementById('N').innerHTML = printResultado
+});
+document.addEventListener('DOMContentLoaded', function(){
+
+    let printResultado = "";
+    let numerosestaticos;
+    let numerosB = [1,2,3,4,5];
+    let numerosI = [2,2,3,4,5];
+    let numerosN = [3,2,3,4,5];
+    let numerosG = [4,2,3,4,5];
+    let numerosO = [5,2,3,4,5];
+    
+    
+    
+    for (numerosestaticos = 0; numerosestaticos < numerosB.length && numerosestaticos < numerosI.length && numerosestaticos < numerosN.length && numerosestaticos < numerosG.length && numerosestaticos < numerosO.length; numerosestaticos++) {
+        numeroGeneradoB = numerosB[numerosestaticos];
+        numeroGeneradoI = numerosI[numerosestaticos];
+        numeroGeneradoN = numerosN[numerosestaticos];
+        numeroGeneradoG = numerosG[numerosestaticos];
+        numeroGeneradoO = numerosO[numerosestaticos];
+    
+    
+    
+        printResultado+=    '<div class="row">'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="b">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoB+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="i">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoI+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="n colortachar">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoN+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="g">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoG+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="o">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoO+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=    '</div>'
+    
+        
+    }   
+    document.getElementById('G').innerHTML = printResultado
+});
+document.addEventListener('DOMContentLoaded', function(){
+
+    let printResultado = "";
+    let numerosB = [1,2,3,4,5];
+    let numerosI = [2,2,3,4,5];
+    let numerosN = [3,2,3,4,5];
+    let numerosG = [4,2,3,4,5];
+    let numerosO = [5,2,3,4,5];
+    
+    
+    
+    for (let numerosestaticos = 0; numerosestaticos < numerosB.length && numerosestaticos < numerosI.length && numerosestaticos < numerosN.length && numerosestaticos < numerosG.length && numerosestaticos < numerosO.length; numerosestaticos++) {
+        numeroGeneradoB = numerosB[numerosestaticos];
+        numeroGeneradoI = numerosI[numerosestaticos];
+        numeroGeneradoN = numerosN[numerosestaticos];
+        numeroGeneradoG = numerosG[numerosestaticos];
+        numeroGeneradoO = numerosO[numerosestaticos];
+    
+    
+    
+        printResultado+=    '<div class="row">'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="b">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoB+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="i">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoI+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="n">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoN+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="g colortachar">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoG+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=        '<div class="col-2">'
+        printResultado+=          '<div class="o">'
+        printResultado+=            '<div class="numero">'+numeroGeneradoO+'</div>'
+        printResultado+=           '</div>'
+        printResultado+=        '</div>'
+        printResultado+=    '</div>'
+    
+        
+    }   
+    document.getElementById('O').innerHTML = printResultado
+    });
